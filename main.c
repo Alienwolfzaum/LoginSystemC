69170d71cd495b3e5603d1759e29588210e55266
Alienwolfzaum <95194022+Alienwolfzaum@users.noreply.github.com>
2026-07-23 15:45:15 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,39 +1,75 @@
 #include <stdio.h>
 
-int main(){
+int opcao = 0;
 
-   
-
-    int opcao = 0;
-
-    while(opcao != 3)
-    {
-
+void montaCabecalho()
+{
     printf("  ========================================\n");
     printf("             LOGIN SYSTEM\n");
     printf("  ========================================\n\n");
+}
 
-         printf(" 1 - Login\n 2 - Cadastro\n 3 - Sair\n\n");
+void montaMenu()
+{
+    printf(" 1 - Login\n 2 - Cadastro\n 3 - Sair\n\n");
+}
 
+int mostraOpcao()
+{
+    int opcao = 0;
+    printf("Digite uma opcao: ");
+    scanf("%d", &opcao);
+    return opcao;
+}
+
+//------------------------------------------------------------------------------------------------------------------------
+
+int main(){
+
+char nome[50];
+char usuario[30];
+char senha[30];
+
+
+    while(opcao != 3)
+    {
+        montaCabecalho();
+        montaMenu();
         printf("Digite uma opcao: ");
         scanf("%d", &opcao);
+        
+        
+        if(opcao == 1)
+        {
+            printf("Voce escolheu Login\n\n");
+        }
+        else if(opcao == 2)
+        {
 
-    if(opcao == 1)
-{
-    printf("Voce escolheu Login\n\n");
-}
-    else if(opcao == 2)
-    {
-        printf("Voce escolheu Cadastro\n\n");
+            printf("Voce escolheu Cadastro\n\n");
+
+            printf("Digite seu nome: ");
+            scanf("%49s", nome);
+            printf("Digite seu usuario: ");
+            scanf("%29s", usuario);
+            printf("Digite sua senha: ");
+            scanf("%29s", senha);
+            printf("\n");
+
+            printf("Nome: %s\n", nome);
+            printf("Usuario: %s\n", usuario);
+            printf("Senha: %s\n", senha);
+
+        }
+        else if(opcao == 3)
+        {
+            printf("Encerrando o programa\n\n");
+        }
+        else
+        {
+            printf("Numero invalido\n\n");
+        }
     }
-    else if(opcao == 3)
-    {
-        printf("Encerrando o programa\n\n");
-    }
-    else
-    {
-        printf("Numero invalido\n\n");
-    }
-}
+    
     return 0;
 }
